0e1f2c4236fbed932e31ff7155699f242f083f36
unsket <unsket@mail.ru>
2022-06-20 22:48:44 +0300

    init router

---
+++ src/screens/CarsList/CarsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface CarsListProps {
+
+}
+
+export const CarsList: React.FC<CarsListProps> = ({}) => {
+    return (
+        <div>Cars List Screen</div>
+    );
+}
